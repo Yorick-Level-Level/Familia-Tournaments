695995f960e4d7be5461677817a8b45cf8ed2feb
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-11-09 21:18:31 +0100

    new image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'Padel Toernooi - Familia',
   description: 'Meld je snel aan voor het leukste padel toernooi van het jaar!',
   openGraph: {
-    images: '/images/og-image.png'
+    images: 'https://img.freepik.com/free-vector/hand-drawn-padel-logo_23-2149205638.jpg?w=826&t=st=1699561089~exp=1699561689~hmac=38563d0e3937ad7d5b874ef57647b99131637e969c72aee6020bfb7316d6b9c5'
   }
 }
 
