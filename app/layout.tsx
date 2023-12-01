8f8614a546fa3482307d4819f112974e98741630
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-12-01 13:14:32 +0100

    fix: meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Padel Toernooi - Familia',
-  description: 'Meld je snel aan voor het leukste padel toernooi van het jaar!',
+  description: 'Het toernooi schema is bekend, bekijk snel wanneer je moet spelen!',
   openGraph: {
     images: 'https://img.freepik.com/free-vector/hand-drawn-padel-logo_23-2149205638.jpg?w=826&t=st=1699561089~exp=1699561689~hmac=38563d0e3937ad7d5b874ef57647b99131637e969c72aee6020bfb7316d6b9c5'
   }
